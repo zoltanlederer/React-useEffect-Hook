fb70638a7bbd1452442cd1da71a0cdcc6f91d6a8
zoltanlederer <zoltanlederer@yahoo.com>
2020-09-16 20:13:25 +0200

    Initial update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect, useRef} from 'react';
 
-function App() {
+/*
+const LogEffect = () => {
+  const [text, setText] = useState('');
+
+  useEffect(() => {
+    console.log('latest value:', text)
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input value={text} onChange={e => setText(e.target.value)} />
     </div>
   );
+};
+*/
+
+/*
+const App = () => {
+  // Store a reference to the input's DOM node
+  const inputRef = useRef();
+
+  // Store the input's value in state
+  const [value, setValue] = useState('');
+
+  useEffect(() => {
+    // This runs AFER the first render,
+    // so the ref is already set
+    console.log('render');
+    inputRef.current.focus();
+  }, [inputRef]);
+
+  return (
+    <input
+      ref={inputRef}
+      value={value}
+      onChange={e => setValue(e.target.value)}
+    />
+  );
+
 }
+*/
+
+/*
+Render an input box and store its value with useState. Then set the document.title in an
+effect, keeping the page’s title in sync with the input.
+*/
+
+const App = () => {
+  const [input, setInput] = useState('1 Title');
+  // console.log(input);
+  
+  useEffect(() => {
+    document.title = input;
+  }, [input]);
+
+  return (
+    <div>
+      {/* <input onChange={e => setInput(e.target.value)} /> */}
+      <input onChange={e => setInput(e.target.value)} />
+    </div>
+  );
+
+};
 
 export default App;
